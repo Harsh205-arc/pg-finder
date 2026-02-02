31e33d6c9992c510f3915e3f1e35b2015828e4fb
Harsh <harshkundra05@gmail.com>
2026-02-02 07:14:46 +0530

    Added DB connection and PG fetch API

---
+++ backend/api/get_pgs.php
@@ -0,0 +1,35 @@
+<?php
+
+ini_set ('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+header ("Content-Type:application/json");
+
+require __DIR__ ."/../config/db.php";
+
+try{
+    $stmt = $pdo ->prepare(
+        "SELECT id,name,rent,deposit, gender, food, distance, verified
+         FROM pgs
+         WHERE verified =1
+         ORDER by distance ASC"
+    );
+
+$stmt->execute();
+$pgs= $stmt->fetchAll();
+
+echo json_encode([
+    "status"=>"success",
+    "data"=> $pgs
+]);
+}
+
+catch(Exception $e){
+    http_response_code(500);
+
+    echo json_encode([
+    "status"=> "Failed",
+    "message"=>"failed to fetch PGs"
+    ]);
+}
